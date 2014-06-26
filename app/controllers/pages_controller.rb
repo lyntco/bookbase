31ec57beb779eb89a845fac24ba70950ecde1f72
lyntco <strawww@gmail.com>
2014-06-26 10:17:07 +1000

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def index
+    @recent_books = Book.order(:release => :desc).limit(5)
+    @recent_authors = Book.order(:release => :desc).map {|book| book.author }.compact.uniq.slice(0,5) #=> an array of 5 unique authors
+  end
+
+  def search
+    search = params[:search]
+    @results = []
+    unless search == ""
+      @results << Author.where("name ILIKE :search", search: "%#{ search }%") # % % means get everything before nd get everything after
+      @results << Author.where("nationality ILIKE :search", search: "%#{ search }%") # ILIKE makes it case insensitive
+      @results << Book.where("title ILIKE :search", search: "%#{ search }%")
+      @results << Book.where("genre ILIKE :search", search: "%#{ search }%")
+      @results = @results.flatten.uniq
+    end
+  end
+
+end
